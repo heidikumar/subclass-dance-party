73e60a2062fbe6dd1ce27733cdc6ff8918d2de3e
heidikumar <heidi.kristine.hansen@gmail.com>
2015-08-26 20:35:26 -0700

    3 dance subclasses; working on lineUp method

--- src/init.js
+++ src/init.js
@@ -25,9 +25,43 @@ $(document).ready(function(){
     var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
-      Math.random() * 2000
+      Math.random() * 10000 //changed to make blinking less intense.
     );
+    window.dancers.push(dancer);
     $('body').append(dancer.$node);
+  });
+
+  $(".lineUp").on("click", function(event){
+    //use getElementsByClassName to find number of starks and lannisters
+    var numStark = document.getElementsByClassName("starkDancer");
+    var numLannister = document.getElementsByClassName("lannisterDancer");
+    //var dancerBuffer = 10;
+    var starkInterval = $("body").height()/numStark.length; 
+    var lannisterInterval = $("body").height()/numLannister.length; 
+    var position; 
+
+    var fixedRightLine = Math.floor($("body").width()*0.75); 
+    var fixedLeftLine = Math.floor($("body").width()*0.25);
+
+    var danceMover; 
+    var top;
+
+/*
+<span class="lannisterDancer" style="top: 419.063794110669px; left: 1618.0654039979px; display: inline;"><img src="http://25.media.tumblr.com/3d25d103e9811f69bd9f9aa022738fa3/tumblr_n0rf4uAHHL1qmz4rgo3_250.gif" style="width:125px;height:125px" <="" span=""></span>
+*/
+    for (var i =0; i<numStark.length; i++){
+      //remove the node and get access to the dancer
+      //danceMover = $(numStark[i]).detach();
+        //reset top and left using object setPosition
+        top = starkInterval*(i+1) + 10;
+      //use setAttribute on a specific HTML tag to change elements within
+      numStark[i].style.top = top;
+      numStark[i].style.left = fixedLeftLine;
+    }
+    for (var j = 0; j<numLannister.length; j++){
+
+    }
+
   });
 });
 
--- src/lannisterDancer.js
+++ src/lannisterDancer.js
@@ -3,7 +3,7 @@
 var makeLannisterDancer = function(top, left, timeBetweenSteps){
   // debugger;
   Dancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('<span class="lannisterDancer"></span>'); 
+  this.$node = $('<span class="lannisterDancer"><img src="http://25.media.tumblr.com/3d25d103e9811f69bd9f9aa022738fa3/tumblr_n0rf4uAHHL1qmz4rgo3_250.gif" style="width:125px;height:125px"</span>'); 
   this.setPosition(this.top, this.left); 
   this.oldStep = this.step; 
   this.stepCount = 0; 
